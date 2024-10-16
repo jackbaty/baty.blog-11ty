65c823fedfeb783eaf2794ecd0edeef314389ca7
Jack Baty <jack@baty.net>
2024-10-16 17:11:10 -0400

    Publish checkpoint

--- content/journals/journals.11tydata.cjs
+++ content/journals/journals.11tydata.cjs
@@ -1,7 +1,4 @@
 module.exports = {
-	tags: [
-		"journal"
-	],
 	"layout": "layouts/post.njk",
 	"permalink": "/{{ page.date.getFullYear() }}/{{ page.date | readableDate('MM') }}/{{ page.date | readableDate('dd') }}/today/",
 };
